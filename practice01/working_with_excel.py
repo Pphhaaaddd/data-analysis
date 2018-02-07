13f62ed2798c37650b28db96d3f403e3337a656f
Suhail Ahamed <suhail511@gmail.com>
2018-02-07 09:04:26 +0400

    Open excel file with xlrd

--- practice01/working_with_excel.py
+++ practice01/working_with_excel.py
@@ -1,1 +1,10 @@
 import xlrd
+workbook = xlrd.open_workbook("May17.xlsx")
+sheet = workbook.sheet_by_index(0)
+print("Number of Rows : ",sheet.nrows)
+print("Number of Columns",sheet.ncols)
+
+for row in range(sheet.nrows):
+    for col in range(sheet.ncols):
+        print(sheet.cell_value(row,col),'\t\t',end='')
+    print()
