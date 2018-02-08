6042e5ec7fc23dda0f5e924f6c561c351128e50e
Suhail Ahamed <suhail511@gmail.com>
2018-02-08 07:47:33 +0400

    add ipython, read excel file pd.ExcelFile(), edit dataframe

--- practice01/working_with_excel.py
+++ practice01/working_with_excel.py
@@ -1,10 +1,18 @@
 import xlrd
+import numpy as np
+import pandas as pd
 workbook = xlrd.open_workbook("May17.xlsx")
 sheet = workbook.sheet_by_index(0)
 print("Number of Rows : ",sheet.nrows)
 print("Number of Columns",sheet.ncols)
 
-for row in range(sheet.nrows):
-    for col in range(sheet.ncols):
-        print(sheet.cell_value(row,col),'\t\t',end='')
-    print()
+
+## Printing the sheet
+# for row in range(sheet.nrows):
+#     for col in range(sheet.ncols):
+#         print(sheet.cell_value(row,col),'\t',end='')
+#     print()
+
+d = [[sheet.cell_value(r,c) for c in range(sheet.ncols) for r in range(sheet.nrows)]]
+df = pd.DataFrame(data=d)
+print(df)
